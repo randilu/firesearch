44c9bbe32b1ffc1287ec9d43fc95ada8855fe428
randilu <randilu.soysa@gmail.com>
2022-07-31 20:20:00 +0530

    add styling

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,20 +1,33 @@
 import type { FC } from "react";
 
-import { Input } from "@material-tailwind/react";
+import { Button, Input, IconButton } from "@material-tailwind/react";
 
 interface SignUpProps {}
 
 export const SignUp: FC<SignUpProps> = () => (
-
-    <div className="flex flex-col justify-center items-center">
-      <div className="w-1/3">
-        <Input variant="static" placeholder="Static" />
+  <div className="flex flex-col justify-center items-center">
+    <div
+      className="flex flex-col items-center w-1/4 rounded-lg bg-white"
+      id="sign-up-box"
+    >
+      <div className="w-5/6 my-4">
+        <Input
+          icon={<i className="fa-user" />}
+          variant="static"
+          placeholder="First Name..."
+        />
       </div>
-      <div className="w-1/3">
-        <Input variant="static" placeholder="Static" />
+      <div className="w-5/6 my-4">
+        <Input variant="static" placeholder="Email..." color="purple" />
       </div>
-      <div className="w-1/3">
-        <Input variant="static" placeholder="Static" />
+      <div className="w-5/6 my-4">
+        <Input variant="static" placeholder="Password" color="purple" />
+      </div>
+      <div className="w-5/6 my-4">
+        <Button variant="text" size="lg">
+          GET STARTED
+        </Button>
       </div>
     </div>
+  </div>
 );
