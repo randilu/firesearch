9a8fcd0e8c1657e8de4997cd0ac8ca820ffd70d9
Sashini Hettiarachchi <sashini.hettiarachchi1@gmail.com>
2022-07-31 22:24:03 +0530

    Add form styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import { initializeApp } from "firebase/app";
 
 // import firebaseConfig from "./configs/firebase-config.json";
 import Login from "../src/components/Login";
-import "./App.scss";
 
 function App() {
   // initializeApp(firebaseConfig.result.sdkConfig);
   return (
-    <div className="App">
+    <div className="h-screen w-screen bg-slate-200" >
       <Login />
     </div>
   );
